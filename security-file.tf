d8d16320d9e3aae91a78d4fea4d24adbd47e2ee9
awimana1 <adelina143graham@gmail.com>
2024-10-10 19:35:03 -0500

    new push

--- security-file.tf
+++ security-file.tf
@@ -1,12 +1,8 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-<<<<<<< HEAD
-  vpc_id      = aws_vpc.main_vpc.id
-=======
-  vpc_id      = aws_vpc.main.id
->>>>>>> tired
 
+  vpc_id      = aws_vpc.main_vpc.id
   tags = {
     Name = "allow_tls"
   }
