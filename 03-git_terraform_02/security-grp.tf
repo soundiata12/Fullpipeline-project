8859f13a166d092c61ee56ea8c3d3242f2d46ad6
tchatoamanda@ <tchatoamanda@gmail.com>
2024-10-11 20:33:04 -0400

    file

---
+++ 03-git_terraform_02/security-grp.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "security_group_id" {
+  name = "security_group_id"
+  vpc_id      =  "Dev_02"
+  tags = {
+    Name = "security_group_id"
+  }
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 443
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 443
+    protocol    = "tcp"
+  }
+}
